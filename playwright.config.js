ec99a4178040aab0580f4a7f51f355663feb5fc1
Tetiana Zekter <tetianazekter@gmail.com>
2026-08-26 15:25:10 +0300

    Configure list and HTML reporters for Playwright, update gitignore

--- playwright.config.js
+++ playwright.config.js
@@ -23,7 +23,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['line'],
+    ['html', { outputFolder: 'playwright-report' }]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     headless: true,
